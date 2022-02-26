23ee4cdfa0fb6c1cfa508350c705081b083037a5
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-02-26 11:27:39 -0800

    updated regex to match email

--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const userSchema = new Schema(
         type: String,
         unique: true,
         required: true,
-        match: [/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/]
+        match: [ /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
       },
       thoughts: [{
           type: Schema.Types.ObjectId,
