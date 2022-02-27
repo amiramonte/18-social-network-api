a87d70597a5397130012c2ef97a0a4c425f16190
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-02-26 16:57:33 -0800

    updated pseudocode.js file to reflect code additions

--- z-pseudocode.js
+++ z-pseudocode.js
@@ -2,9 +2,6 @@
     // getter method to format the timestamp on query
 
 
-// conduct proper imports to index.js file in controllers folder
-// proper imports to api/index.js file from controllers index.js file
-// proper imports to user-routes and thought-routes folders within api folder
 // 
 // 
 // 
