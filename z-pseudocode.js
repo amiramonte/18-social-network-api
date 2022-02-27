f26a831bd32d24759df9c87b0d503a606a9bfcad
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-02-26 17:04:17 -0800

    updated pseudocode to reflect next steps

--- z-pseudocode.js
+++ z-pseudocode.js
@@ -2,24 +2,24 @@
     // getter method to format the timestamp on query
 
 
+// API/USER ROUTES
+    // Post route to create a new user
+    // Get route for all users
+    // Get route for single user by its _id and populated thought and friend data
+    // Put route to update a user by its _id
+    // Delete route to delete a user by its _id
+    // 
+    // 
+    // 
+    // 
 // 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+// API/THOUGHTS ROUTES
+    // 
+    // 
+    // 
+    // 
+    // 
+    // 
+    // 
 // 
 // 
